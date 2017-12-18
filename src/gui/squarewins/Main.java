b0d554ce9adfc067f0f1b0b574905b2c4e4bb64b
AndyNope <andy.bui@stud.kbw.ch>
2017-12-18 09:25:51 +0100

    Awesome Update 1.0.0 Alpha

--- src/gui/squarewins/Main.java
+++ src/gui/squarewins/Main.java
@@ -3,6 +3,7 @@ package gui.squarewins;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -21,7 +22,7 @@ public class Main extends Application {
 //        Group root = new Group();
 
         Scene scene = new Scene(root);
-
+        stage.setTitle("Square Wins");
         stage.setScene(scene);
         stage.show();
 
